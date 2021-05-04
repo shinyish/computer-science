525c35074a850ff1f0e208ecb46dd0693bcb40e1
shiny <57299745+shinyish@users.noreply.github.com>
2021-05-03 19:35:01 -0700

    Create window.java

---
+++ Java/window.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class index extends JFrame {
+    public index() {
+        setTitle("Ch3ckSplo1t");
+        setSize(400, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        index idk = new index();
+    }
+}
