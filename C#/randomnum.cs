56ca07b77fa15cd5d05c65b5204a5aef41688905
shiny <57299745+shinyish@users.noreply.github.com>
2021-06-20 17:29:59 -0700

    Update randomnum.cs

--- C#/randomnum.cs
+++ C#/randomnum.cs
@@ -1,2 +1,8 @@
-Random rnd = new Random();
-int number = rnd.Next(0,100) // change the first number to the minimum number you want to generate and the second number the max you want to generate
+namepsace randomnum {
+  public static class random {
+    public static void Main(string[] args) {
+      Random rnd = new Random();
+      int number = rnd.Next(0,100) // change the first number to the minimum number you want to generate and the second number the max you want to generate
+    }
+  }
+}
